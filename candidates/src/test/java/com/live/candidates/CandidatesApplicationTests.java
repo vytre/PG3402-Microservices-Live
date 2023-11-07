2006ee4d9ac839f7c0b0f114c277404efd3f25bb
vytre <vegaytre@gmail.com>
2023-11-07 05:01:48 +0100

    Added candidates module

---
+++ candidates/src/test/java/com/live/candidates/CandidatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.live.candidates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
